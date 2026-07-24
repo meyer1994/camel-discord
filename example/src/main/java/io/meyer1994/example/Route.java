3ce419da11ae804fa7d08f66f09675002ef2a535
meyer1994 <1994meyer@gmail.com>
2026-07-24 20:32:43 -0300

    Update example routes for named endpoints

--- example/src/main/java/io/meyer1994/example/Route.java
+++ example/src/main/java/io/meyer1994/example/Route.java
@@ -5,60 +5,60 @@ import org.apache.camel.builder.RouteBuilder;
 public class Route extends RouteBuilder {
         @Override
         public void configure() throws Exception {
-                from("discord:onMessageReceived")
+                from("discord:messages?event=onMessageReceived")
                                 .log("Message received: ${body.message.contentRaw}")
                                 .choice()
                                 .when().simple("${body.message.contentRaw} == '!ping'")
                                 .log("Responding to !ping")
                                 .transform().constant("Pong!")
-                                .to("discord:sendMessage")
+                                .to("discord:messages?operation=sendMessage")
                                 .when().simple("${body.message.contentRaw} == '!nice'")
                                 .log("Reacting to !nice")
                                 .split()
                                 .constant("🇳,🇮,🇨,🇪")
                                 .delimiter(",")
-                                .to("discord:addReactionById")
+                                .to("discord:messages?operation=addReactionById")
                                 .end()
                                 .end();
 
-                from("discord:onMessageReceived")
+                from("discord:messages?event=onMessageReceived")
                                 .log("Message received: ${body.message.contentRaw}")
                                 .transform().simple("log.onMessageReceived: ${body.message}")
                                 .to("sql:insert into event(data) values (${body.message})");
 
-                from("discord:onMessageUpdate")
+                from("discord:message-updates?event=onMessageUpdate")
                                 .log("Message updated: ${body.message.contentRaw}")
                                 .transform().simple("log.onMessageUpdate: ${body.message}")
-                                .to("discord:sendMessage");
+                                .to("discord:messages?operation=sendMessage");
 
-                from("discord:onMessageDelete")
+                from("discord:message-deletions?event=onMessageDelete")
                                 .log("Message deleted: ${body.messageId}")
                                 .transform().simple("log.onMessageDelete: ${body.messageId}")
-                                .to("discord:sendMessage");
+                                .to("discord:messages?operation=sendMessage");
 
-                from("discord:onMessageBulkDelete")
+                from("discord:message-bulk-deletions?event=onMessageBulkDelete")
                                 .log("Messages bulk deleted: ${body.messageIds}")
                                 .transform().simple("log.onMessageBulkDelete: ${body.messageIds}")
-                                .to("discord:sendMessage");
+                                .to("discord:messages?operation=sendMessage");
 
-                from("discord:onMessageReactionAdd")
+                from("discord:reaction-additions?event=onMessageReactionAdd")
                                 .log("Reaction added: ${body.reaction}")
                                 .transform().simple("log.onMessageReactionAdd: emoji=${body.reaction.emoji}")
-                                .to("discord:sendMessage");
+                                .to("discord:messages?operation=sendMessage");
 
-                from("discord:onMessageReactionRemove")
+                from("discord:reaction-removals?event=onMessageReactionRemove")
                                 .log("Reaction removed: ${body.reaction}")
                                 .transform().simple("log.onMessageReactionRemove: emoji=${body.reaction.emoji}")
-                                .to("discord:sendMessage");
+                                .to("discord:messages?operation=sendMessage");
 
-                from("discord:onMessageReactionRemoveAll")
+                from("discord:reaction-remove-all?event=onMessageReactionRemoveAll")
                                 .log("All reactions removed from message: ${body.messageId}")
                                 .transform().simple("log.onMessageReactionRemoveAll: messageId=${body.messageId}")
-                                .to("discord:sendMessage");
+                                .to("discord:messages?operation=sendMessage");
 
-                from("discord:onMessageReactionRemoveEmoji")
+                from("discord:reaction-remove-emoji?event=onMessageReactionRemoveEmoji")
                                 .log("Reaction emoji removed: ${body.emoji}")
                                 .transform().simple("log.onMessageReactionRemoveEmoji: emoji=${body.emoji}")
-                                .to("discord:sendMessage");
+                                .to("discord:messages?operation=sendMessage");
         }
 }
