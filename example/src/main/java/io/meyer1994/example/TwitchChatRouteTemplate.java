cd29a142529e57835a39def62ee3cf11cc4fcf38
meyer1994 <1994meyer@gmail.com>
2026-07-27 21:44:55 -0300

    Add multi-stage Dockerfile for example

--- example/src/main/java/io/meyer1994/example/TwitchChatRouteTemplate.java
+++ example/src/main/java/io/meyer1994/example/TwitchChatRouteTemplate.java
@@ -22,44 +22,39 @@ public class TwitchChatRouteTemplate extends RouteBuilder {
         .bean(TwitchStreamService.class, "publish");
 
     for (String channel : channels) {
-      from("twitch:%s?event=STREAM_ONLINE".formatted(channel))
-          .log("Stream online: ${body.type}:${body.id}");
-      from("twitch:%s?event=STREAM_OFFLINE".formatted(channel))
-          .log("Stream offline: ${body.type}:${body.id}");
+      from("twitch:%s?event=CHAT".formatted(channel))
+          .log("Inserting message into database: ${body.messageEvent.messageId}")
+          .to("""
+              sql:INSERT INTO twitch_event_chat (
+                message_id,
+                event_time,
+                channel_id,
+                channel_name,
+                user_id,
+                user_name,
+                display_name,
+                message,
+                subscriber_months,
+                subscription_tier,
+                nonce,
+                raw_event
+              ) VALUES (
+                :#${body.messageEvent.messageId.orElse(null)},
+                :#${body.messageEvent.firedAtInstant.atOffset('Z')},
+                :#${body.channel.id},
+                :#${body.channel.name},
+                :#${body.user.id},
+                :#${body.user.name},
+                :#${body.messageEvent.userDisplayName.orElse(null)},
+                :#${body.message},
+                :#${body.subscriberMonths},
+                :#${body.subscriptionTier},
+                :#${body.nonce},
+                CAST('{}' AS jsonb)
+              )
+              """)
+          .to("seda:twitch-chat-listener");
     }
-
-    from("twitch:aspaszin?event=CHAT")
-        .log("Inserting message into database: ${body.messageEvent.messageId}")
-        .to("""
-            sql:INSERT INTO twitch_event_chat (
-              message_id,
-              event_time,
-              channel_id,
-              channel_name,
-              user_id,
-              user_name,
-              display_name,
-              message,
-              subscriber_months,
-              subscription_tier,
-              nonce,
-              raw_event
-            ) VALUES (
-              :#${body.messageEvent.messageId.orElse(null)},
-              :#${body.messageEvent.firedAtInstant.atOffset('Z')},
-              :#${body.channel.id},
-              :#${body.channel.name},
-              :#${body.user.id},
-              :#${body.user.name},
-              :#${body.messageEvent.userDisplayName.orElse(null)},
-              :#${body.message},
-              :#${body.subscriberMonths},
-              :#${body.subscriptionTier},
-              :#${body.nonce},
-              CAST('{}' AS jsonb)
-            )
-            """)
-        .to("seda:twitch-chat-listener");
 
     from("seda:twitch-chat-listener")
         .log("Published message to SEDA: ${body.messageEvent.messageId}");
